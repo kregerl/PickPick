bf096b05dd8c69c77162a15ec0f85713294d2e94
Loucas <Loucas@DESKTOP-RHGFJTA>
2020-07-29 20:58:41 -0400

    Changed how tool is chosen, now based on item tier, then enchantment.

--- src/main/java/com/loucaskreger/pickpick/PickPick.java
+++ src/main/java/com/loucaskreger/pickpick/PickPick.java
@@ -1,5 +1,8 @@
 package com.loucaskreger.pickpick;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import com.loucaskreger.pickpick.config.Config;
 
 
@@ -14,6 +17,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod("pickpick")
 public class PickPick {
 	public static final String MODID = "pickpick";
+	public static final Logger LOGGER = LogManager.getLogger();
 
 	public PickPick() {
 		final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
